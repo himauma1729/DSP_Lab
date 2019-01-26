a9bc349f1a9763c2edd41857043c7684fd3094a9
himauma1729 <46996647+himauma1729@users.noreply.github.com>
2019-01-26 22:02:43 +0530

    Add files via upload

---
+++ Lab_prog3(Sinusoidals)_24thJan19.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  5 15:17:02 2019
+
+@author: rgukt
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.linspace(0,0.01,100)
+f=input("f:")
+fs=input("fs:")
+y=np.sin(2*np.pi*f*x)
+plt.plot(x,y)
+plt.show()
+z=np.sin(2*np.pi*fs*x)
+plt.plot(x,z)
+plt.show()
+c=y+z
+plt.plot(x,c)
+plt.show()
